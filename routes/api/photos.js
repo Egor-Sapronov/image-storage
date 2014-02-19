62b20c976d49dd7edf0cc42e4348c4540d95da0d
Egor <sapronov.egor@gmail.com>
2014-02-19 17:24:53 +0700

    Add empty CRUD API for photos.

---
+++ routes/api/photos.js
@@ -0,0 +1,20 @@
+exports.get = function (req, res) { // api/photos
+    res.send('This is not implemented now');
+};
+
+exports.post = function (req, res) { // api/photos
+    res.send('This is not implemented now');
+};
+
+exports.getById = function (req, res) { // api/photos:id
+    res.send('This is not implemented now');
+};
+
+exports.put = function (req, res) { // api/photos:id
+    res.send('This is not implemented now');
+};
+
+exports.delete = function (req, res) { // api/photos:id
+    res.send('This is not implemented now');
+};
+
