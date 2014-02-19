886a2339a5512d89f7ab6973dd9634f2260e256d
Egor <sapronov.egor@gmail.com>
2014-02-19 19:14:03 +0700

    Small fix.

--- libs/log.js
+++ libs/log.js
@@ -1,17 +1,17 @@
-var winston = require('winston');
+var winston=require('winston');
 
 function getLogger(module){
-    var path=module.fileName.split('/').slice(-2).join('/');
+    var path=module.filename.split('/').slice(-2).join('/');
 
-    return new winston.logger({
-        transport:[
-            new winston.transports.Console({
-                colorize:true,
-                level:'debug',
-                label:path
-            })
-        ]
-    })
+    return new (winston.Logger)({
+       transports:[
+       new winston.transports.Console({
+           colorize:true,
+           level:'debug',
+           label:path
+       })
+       ]
+    });
 }
 
-module.exports=getLogger;
+module.exports = getLogger;
