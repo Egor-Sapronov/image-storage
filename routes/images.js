93522c4922eb5a48ae1a68ec41b8e6dda8f006ed
Egor <sapronov.egor@gmail.com>
2014-02-22 16:35:48 +0700

    Add actions to image upload.

--- routes/images.js
+++ routes/images.js
@@ -1,3 +1,8 @@
+var Image = require('../models/mongo');
+var path = require('path');
+var fs = require('fs');
+var join = path.join;
+
 var photos = [];
 
 exports.list = function (req, res) {
@@ -5,4 +10,30 @@ exports.list = function (req, res) {
         title: 'Photos',
         photos: photos
     });
+};
+
+exports.form = function (req, res) {
+    res.render('images/upload', {
+        title: 'Image upload'
+    });
+};
+
+exports.submit = function (dir) {
+    return function (req, res, next) {
+        var img = req.files.image.file;
+        var name = req.body.image.name || img.name;
+        var path = join(dir, img.name);
+
+        fs.rename(img.path, path, function (err) {
+            if (err) return next(err);
+
+            Image.create({
+                name: name,
+                path: img.name
+            }, function (err) {
+                if (err) return next(err);
+                res.redirect('/');
+            });
+        });
+    };
 };
