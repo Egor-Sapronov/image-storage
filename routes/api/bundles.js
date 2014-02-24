750f57c3cd74d8c9d73eecc7c502ff5080e71ef3
Egor <sapronov.egor@gmail.com>
2014-02-24 17:49:47 +0700

    Add empty CRUD actions for bundles API.

---
+++ routes/api/bundles.js
@@ -0,0 +1,22 @@
+var Bundle = require('../../models/bundle');
+var log = require('../../libs/log')(module);
+
+exports.get = function (req, res) {
+    res.send('Not implemented.');
+};
+
+exports.post = function (req, res) {
+    res.send('Not implemented.');
+};
+
+exports.getById = function (req, res) {
+    res.send('Not implemented.');
+};
+
+exports.put = function (req, res) {
+    res.send('Not implemented.');
+};
+
+exports.delete = function (req, res) {
+    res.send('Not implemented.');
+};
