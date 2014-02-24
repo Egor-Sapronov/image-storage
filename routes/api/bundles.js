70f452f609c7992ff935d1519011243f735e500c
Egor <sapronov.egor@gmail.com>
2014-02-24 18:15:49 +0700

    Add get and getBuId actions.

--- routes/api/bundles.js
+++ routes/api/bundles.js
@@ -1,8 +1,15 @@
-var Bundle  = require('../../models/models').Bundle;
-var log     = require('../../libs/log')(module);
+var Bundle = require('../../models/models').Bundle;
+var log = require('../../libs/log')(module);
 
 exports.get = function (req, res) {
-    res.send('Not implemented.');
+    Bundle.find({}, function (err, bundles) {
+        if (!err) return res.send(bundles);
+        else {
+            res.statusCode = 500;
+            log.error('Internal error(%d): %s', res.statusCode, err.message);
+            return res.send({ error: 'Server error' });
+        }
+    });
 };
 
 exports.post = function (req, res) {
@@ -10,7 +17,19 @@ exports.post = function (req, res) {
 };
 
 exports.getById = function (req, res) {
-    res.send('Not implemented.');
+    return Bundle.findById(req.params.id, function (err, bundle) {
+        if (!bundle) {
+            res.statusCode = 500;
+            return res.send({error: 'Not found'});
+        }
+        if (!err) {
+            return res.send({status: 'OK', bundle: bundle});
+        } else {
+            res.statusCode = 500;
+            log.error('Internal error(%d): %s', res.statusCode, err.message);
+            return res.send({ error: 'Server error' });
+        }
+    });
 };
 
 exports.put = function (req, res) {
