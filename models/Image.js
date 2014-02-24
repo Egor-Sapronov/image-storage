b6184ef98384ad0989e28b84cd62582403e34cdf
Egor <sapronov.egor@gmail.com>
2014-02-24 17:49:04 +0700

    Small update.

--- models/Image.js
+++ models/Image.js
@@ -3,8 +3,8 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/image_storage');
 
 var image = new mongoose.Schema({
-    name:String,
-    path:String
+    name: String,
+    path: String
 });
 
-module.exports=mongoose.model('image',image);
+module.exports = mongoose.model('image', image);
