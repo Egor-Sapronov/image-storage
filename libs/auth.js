3979836c7e043b2207a4c0e4955155c4e5c6e103
Egor <sapronov.egor@gmail.com>
2014-03-02 18:53:36 +0700

    Testig passport.js

---
+++ libs/auth.js
@@ -0,0 +1,18 @@
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password'
+}, function (username, password, done) {
+    User.findOne({username: username}, function (err, user) {
+        return err
+            ? done(err)
+            : user
+            ? password === user.password
+            ? done(null, user)
+            : done(null, false, {message: 'Incorrect password'})
+            : done(null, false, {message: 'Incorrect username'
+        });
+    });
+}));
