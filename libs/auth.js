8ed2a4c001fa21e374ab89c8860ceabd6d139852
Egor <sapronov.egor@gmail.com>
2014-03-03 18:14:25 +0700

    Testing passport.js

--- libs/auth.js
+++ libs/auth.js
@@ -1,5 +1,6 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/models').User;
 
 passport.use(new LocalStrategy(function (username, password, done) {
     User.findOne({username: username}, function (err, user) {
