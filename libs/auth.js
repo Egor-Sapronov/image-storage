bb876aa7132b9cecb0ae48e00fddf5cb74b7f0bc
Egor <sapronov.egor@gmail.com>
2014-03-03 17:35:30 +0700

    Add local strategy. Testing passportjs.

--- libs/auth.js
+++ libs/auth.js
@@ -1,18 +1,18 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new LocalStrategy({
-    usernameField: 'username',
-    passwordField: 'password'
-}, function (username, password, done) {
+passport.use(new LocalStrategy(function (username, password, done) {
     User.findOne({username: username}, function (err, user) {
-        return err
-            ? done(err)
-            : user
-            ? password === user.password
-            ? done(null, user)
-            : done(null, false, {message: 'Incorrect password'})
-            : done(null, false, {message: 'Incorrect username'
-        });
+        if (err) {
+            return done(err);
+        }
+        if (!user) {
+            return done(null, false, {message: 'Incorrecr username.'});
+        }
+        if (!user.validPassword(password)) {
+            return done(null, false, {message: 'Incorrect password.'});
+        }
+
+        return done(null, user);
     });
 }));
