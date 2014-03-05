bb74c1d210508c0068a1b8db4c65606ff1d34e86
Egor <sapronov.egor@gmail.com>
2014-03-05 14:42:04 +0700

    Add exchange refreshToken for access token.

--- libs/oauth2.js
+++ libs/oauth2.js
@@ -1,6 +1,6 @@
+var oauth2rize = require('oauth2rize');
 var config = require('./config');
 var passport = require('passport');
-var oauth2rize = require('oauth2rize');
 var crypto = require('crypto');
 var UserModel = require('../models/models').User;
 var ClientModel = require('../models/models').Client;
@@ -9,7 +9,7 @@ var RefreshTokenModel = require('../models/models').RefreshToken;
 
 var server = oauth2rize.createServer();
 
-// Exhange username & password for access token.
+// Exchange username & password for access token.
 server.exchange(oauth2rize.exchange.password(function (client, username, password, scope, done) {
     UserModel.findOne({username: username}, function (err, user) {
         if (err) {
@@ -48,6 +48,51 @@ server.exchange(oauth2rize.exchange.password(function (client, username, passwor
                 return done(err);
             }
             done(null, tokenValue, refreshTokenValue, {'expires_in': config.get('security.tokenLife')});
+        });
+    });
+}));
+
+// Exchange refreshToken for access token.
+server.exchange(oauth2rize.exchange.refreshToken(function(client,refreshToken,scope,done){
+    RefreshTokenModel.findOne({token:refreshToken},function(err,token){
+        if (err) {
+            return done(err);
+        }
+        if (!token) {
+            return done(null, false);
+        }
+
+        UserModel.findById(token.userId,function(err,user){
+            if(err){return done(err);}
+            if(!user){return done(null,false);}
+
+            RefreshTokenModel.remove({userId: user.userId, clientId: client.clientId}, function (err) {
+                if (err) {
+                    return done(err);
+                }
+            });
+            AccessTokenModel.remove({userId: user.userId, clientId: client.clientId}, function (err) {
+                if (err) {
+                    return done(err);
+                }
+            });
+
+            var tokenValue = crypto.randomBytes(32).toString('base64');
+            var refreshTokenValue = crypto.randomBytes(32).toString('base64');
+            var token = new AccessTokenModel({token: tokenValue, clientId: client.clientId, userId: user.userId});
+            var refreshToken = new RefreshTokenModel({token: refreshTokenValue, clientId: client.clientId, userId: user.userId});
+            refreshToken.save(function (err) {
+                if (err) {
+                    return done(err);
+                }
+            });
+            var info = {scope: '*'}
+            token.save(function (err, token) {
+                if (err) {
+                    return done(err);
+                }
+                done(null, tokenValue, refreshTokenValue, {'expires_in': config.get('security.tokenLife')});
+            });
         });
     });
 }));
