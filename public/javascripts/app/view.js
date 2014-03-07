35b5966cf99d9a0646c0d66ebf2a841ef5b82e23
Egor <sapronov.egor@gmail.com>
2014-03-07 15:11:34 +0700

    Delete bad code.

--- public/javascripts/app/view.js
+++ public/javascripts/app/view.js
@@ -1,27 +1,2 @@
-var Views = {};
 
-var Upload = Backbone.View.extend({
-    el: $('#block'),
-
-    template: _.template($('#upload').html()),
-
-    render: function () {
-        $(this.el).html(this.template());
-    }
-});
-
-var Images = Backbone.View.extend({
-    el: $('#block'),
-
-    template: _.template($('#images').html()),
-
-    render: function () {
-        $(this.el).html(this.template());
-    }
-});
-
-Views = {
-    upload: new Upload(),
-    images: new Images()
-};
 
