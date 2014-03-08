f2b06bae5839dbad79f00845a00a7a88ae3d454d
Egor <sapronov.egor@gmail.com>
2014-03-08 16:24:24 +0700

    Add navigation between images and bundles.

--- public/javascripts/app/controller.js
+++ public/javascripts/app/controller.js
@@ -1,1 +1,21 @@
+var Controller = Backbone.Router.extend({
+    routes: {
+        '': 'images',
+        '!/': 'images',
+        '!/bundles':'bundles'
+    },
+    images: function () {
+        if (Views.images != null) {
+            Views.images.render();
+        }
+    },
+    bundles: function () {
+        if (Views.bundles != null) {
+            Views.bundles.render();
+        }
+    }
+});
 
+var controller = new Controller();
+
+Backbone.history.start();
--- public/javascripts/app/view.js
+++ public/javascripts/app/view.js
@@ -1,2 +1,26 @@
+var Views = {};
 
+var Images = Backbone.View.extend({
+    el: $('#main'),
 
+    template: _.template($('#images').html()),
+
+    render: function () {
+        $(this.el).html(this.template());
+    }
+});
+
+var Bundles = Backbone.View.extend({
+    el: $('#main'),
+
+    template: _.template($('#bundles').html()),
+
+    render: function () {
+        $(this.el).html(this.template());
+    }
+});
+
+Views = {
+    images: new Images(),
+    bundles: new Bundles
+};
