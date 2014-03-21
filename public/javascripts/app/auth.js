68ee4d2aec2fb0c8898c55da8d0532a631b3ba01
Egor <sapronov.egor@gmail.com>
2014-03-21 17:16:19 +0700

    Small update.

--- public/javascripts/app/auth.js
+++ public/javascripts/app/auth.js
@@ -11,9 +11,9 @@ var auth = {
     }
 };
 
-$(document).ready(function () {
-    $.ajaxSetup({
-        headers: { 'Authorization': 'Bearer ' + auth.getAccessToken() }
-    });
+
+$.ajaxSetup({
+    headers: { 'Authorization': 'Bearer ' + auth.getAccessToken() }
 });
+
 
