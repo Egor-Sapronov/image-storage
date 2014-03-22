c2a513609d7ac84e68d3b8e538bd38e792210c63
Egor <sapronov.egor@gmail.com>
2014-03-22 15:28:14 +0700

    Add user models.

---
+++ public/javascripts/models/logInModel.js
@@ -0,0 +1,8 @@
+var LogInModel = Backbone.Model.extend({
+    defaults: {
+        'grant_type': 'password',
+        'client_id': 'webV1',
+        'client_secret': 'abc123456'
+    },
+    url: 'oauth/token'
+});
--- public/javascripts/views/auth.js
+++ public/javascripts/views/auth.js
@@ -1,9 +1,16 @@
 var AuthView = Backbone.View.extend({
     el: $('#auth'),
 
-    template: _.template($('#logIn').html()),
+    logIn: new LogInModel(),
+
+    user: new UserModel(),
+
+    templates: {
+        'logIn': _.template($('#logIn').html()),
+        'loggedIn': _.template($('#loggedIn').html())
+    },
 
     render: function () {
-        $(this.el).html(this.template());
+        $(this.el).html(this.templates['logIn']());
     }
 });
