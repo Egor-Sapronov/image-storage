fc1a275e6513c314044f2909dbfa3c72cc8b1a47
Egor <sapronov.egor@gmail.com>
2014-03-26 15:30:34 +0700

    Add route to images view.

--- public/javascripts/controllers/controller.js
+++ public/javascripts/controllers/controller.js
@@ -1,4 +1,16 @@
 var Controller = Backbone.Router.extend({
+    routes: {
+        '': 'images',
+        '!/': 'images',
+        '!/images': 'images'
+    },
+
+    images: function () {
+        if (Views.images != null) {
+            Views.images.render();
+        }
+    },
+
     initialize: function () {
         if (Views.navBar != null) {
             Views.navBar.render();
